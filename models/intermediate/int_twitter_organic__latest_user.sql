91dce2bdfa662728e28b61bfa78ad985c55fd8e5
Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>
2024-08-19 14:31:55 -0500

    macro updates

--- models/intermediate/int_twitter_organic__latest_user.sql
+++ models/intermediate/int_twitter_organic__latest_user.sql
@@ -7,7 +7,11 @@ with users as (
 
     select 
         *,
-        row_number() over (partition by user_id {{ ", source_relation" if var('twitter_organic_union_schemas', []) or var('twitter_organic_union_databases', [])  | length > 1 }} order by _fivetran_synced desc) = 1 as is_most_recent_record
+        {{ twitter_organic.window_function_if_table_exists(
+            table_ref=var('users_staging'), 
+            partition_statement='row_number() over (partition by user_id' ~ (', source_relation' if var('twitter_organic_union_schemas', []) or var('twitter_organic_union_databases', []) | length > 1) ~ ' order by _fivetran_synced desc)',
+            if_empty=1
+        )}} = 1 as is_most_recent_record
     from users
 
 )
