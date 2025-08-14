abbefdd22b5531c8054af0b0a554b32002e9e334
Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>
2025-08-13 20:34:41 -0500

    minor updates

--- models/intermediate/int_twitter_organic__latest_account.sql
+++ models/intermediate/int_twitter_organic__latest_account.sql
@@ -1,14 +1,14 @@
 with accounts as (
 
     select *
-    from {{ ref('twitter_organic__account_history') }}
+    from {{ ref('stg_twitter_organic__organic_tweet_report') }}
 
 ), is_most_recent as (
 
     select 
         *,
         {{ twitter_organic.result_if_table_exists(
-            table_ref=ref('twitter_organic__account_history'), 
+            table_ref=ref('stg_twitter_organic__organic_tweet_report'), 
             result_statement='row_number() over (partition by account_id' ~ (', source_relation' if var('twitter_organic_union_schemas', []) or var('twitter_organic_union_databases', []) | length > 1) ~ ' order by _fivetran_synced desc)',
             if_empty=1
         )}} = 1 as is_most_recent_record
